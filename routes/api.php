bc4148e1756363b7ae34978dde58c8438f0b1f3f
yanmyoe <yanmyoe@gmail.com>
2022-03-14 15:36:05 +0630

    add test

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function () {
+    return 'Hello World';
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
